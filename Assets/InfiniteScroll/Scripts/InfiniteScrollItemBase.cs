ff93944c0fe5bf21a0bf1f27a7ba532ce711769f
tetr4lab <48040768+tetr4lab@users.noreply.github.com>
2023-02-09 14:06:55 +0900

    クラスを分離

---
+++ Assets/InfiniteScroll/Scripts/InfiniteScrollItemBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace InfiniteScroll {
+
+    /// <summary>
+    /// 論理項目(抽象クラス)
+    ///   継承したクラスを用意して、スクロールレクトの初期化に使用する
+    ///   論理項目のリストの一部が物理項目に反映される
+    /// </summary>
+    public abstract class InfiniteScrollItemBase {
+
+        /// <summary>
+        /// 物理項目を生成
+        ///   GameObjectを生成して、InfiniteScrollItemComponentを継承したコンポーネントをアタッチする
+        /// </summary>
+        /// <returns>生成したGameObjectにアタッチされているコンポーネントを返す</returns>
+        public virtual InfiniteScrollItemComponentBase Create (InfiniteScrollRect scrollRect, int index) {
+            var component = InfiniteScrollItemComponentBase.Create (scrollRect, index);
+            return component;
+        }
+
+        /// <summary>スクロール方向の位置 (実態へ反映)</summary>
+        public virtual float Position { get; protected internal set; }
+
+        /// <summary>スクロール方向のサイズ (物理項目から反映)</summary>
+        public virtual float Size { get; protected internal set; }
+
+        /// <summary>内容に変更があった</summary>
+        public virtual bool Dirty { get; protected internal set; }
+
+        /// <summary>コンストラクタ</summary>
+        public InfiniteScrollItemBase () { }
+
+        /// <summary>文字列化</summary>
+        public override string ToString () => $"{base.ToString ()}({Position}, {Size}, {Dirty})";
+
+    }
+
+}
---
+++ Assets/InfiniteScroll/Scripts/InfiniteScrollItemComponentBase.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace InfiniteScroll {
+
+    /// <summary>
+    /// 物理項目(抽象クラス)
+    ///   このクラスを継承したクラスを、Contentに配置するGameObjectにアタッチする
+    ///   論理項目と物理項目(これ)を同期する役割を担う
+    /// </summary>
+    [DisallowMultipleComponent]
+    [RequireComponent (typeof (RectTransform))]
+    public abstract class InfiniteScrollItemComponentBase : MonoBehaviour {
+
+        #region static
+
+        /// <summary>レイアウトを確定するまでの最大ビルド回数</summary>
+        protected const int MaxNumberOfLayoutRebuilds = 10;
+
+        /// <summary>コンポーネント付きオブジェクトの生成</summary>
+        public static InfiniteScrollItemComponentBase Create (InfiniteScrollRect scrollRect, int index) {
+            throw new NotImplementedException ("Must be implemented in a derived class");
+#pragma warning disable CS0162 // 到達できないコードが検出されました
+            // 以下参考
+            var obj = new GameObject ("InfiniteScrollItem");
+            var rect = obj.AddComponent<RectTransform> ();
+            obj.transform.SetParent (scrollRect.content);
+            var component = (obj.GetComponent<InfiniteScrollItemComponentBase> () ?? obj.AddComponent<InfiniteScrollItemComponentBase> ());
+            component.ScrollRect = scrollRect;
+            component.Index = index;
+            component.Initialize ();
+            return component;
+#pragma warning restore CS0162 // 到達できないコードが検出されました
+        }
+
+        #endregion
+
+        /// <summary>初期化</summary>
+        protected virtual void Initialize () { }
+
+        /// <summary>親のスクロールレクト</summary>
+        public virtual InfiniteScrollRect ScrollRect { get; protected set; }
+
+        /// <summary>項目のパディング</summary>
+        protected virtual RectOffset m_padding => ScrollRect.m_padding;
+
+        /// <summary>項目のサイズを制御</summary>
+        protected virtual bool m_controlChildSize => ScrollRect.m_controlChildSize;
+
+        /// <summary>項目の配向</summary>
+        protected virtual TextAnchor m_childAlignment => ScrollRect.m_childAlignment;
+
+        /// <summary>逆並び</summary>
+        protected virtual bool m_reverseArrangement => ScrollRect.m_reverseArrangement;
+
+        /// <summary>スクロールの向き 垂直/!水平</summary>
+        protected virtual bool vertical => ScrollRect.vertical;
+
+        /// <summary>ビューポート矩形</summary>
+        protected virtual Rect viewportRect => ScrollRect.viewport.rect;
+
+        /// <summary>レクトトランスフォーム</summary>
+        protected virtual RectTransform RectTransform => _rectTransform ?? transform as RectTransform ?? gameObject.AddComponent<RectTransform> ();
+        protected RectTransform _rectTransform;
+
+        /// <summary>サイズ</summary>
+        public virtual float Size => ScrollRect.vertical ? RectTransform.rect.height : RectTransform.rect.width;
+
+        /// <summary>リンク中の論理項目インデックス</summary>
+        public virtual int Index {
+            get => _index;
+            set {
+                _index = value;
+                if (value >= 0) {
+                    Apply ();
+                    SetPosition (Item.Position);
+                    gameObject.SetActive (true);
+                } else {
+                    gameObject.SetActive (false);
+                }
+            }
+        }
+        protected int _index;
+
+        /// <summary>リンク中の論理項目</summary>
+        public virtual InfiniteScrollItemBase Item => (_index < 0 || _index >= ScrollRect.Count) ? null : ScrollRect [_index];
+
+        /// <summary>論理項目の状態を反映</summary>
+        protected virtual void Apply () => Item.Dirty = false;
+
+        /// <summary>更新</summary>
+        protected virtual void Update () {
+            if (ScrollRect && !ScrollRect.LockUpdate && Index >= 0) {
+                if (Item.Dirty) {
+                    Apply ();
+                }
+                if (Item.Size != Size) {
+                    var locked = ScrollRect.GetScroll ();
+                    Item.Size = Size;
+                    ScrollRect.CalculatePositions ();
+                    ScrollRect.SetScroll (locked);
+                }
+            }
+        }
+
+        /// <summary>項目のサイズ決め</summary>
+        protected internal virtual void SetSize () {
+            if (m_controlChildSize) {
+                RectTransform.sizeDelta = vertical
+                    ? new Vector2 (viewportRect.size.x - m_padding.left - m_padding.right, RectTransform.sizeDelta.y)
+                    : new Vector2 (RectTransform.sizeDelta.x, viewportRect.size.y - m_padding.top - m_padding.bottom);
+            }
+            for (var i = 0; i < MaxNumberOfLayoutRebuilds; i++) {
+                var lastSize = Size;
+                LayoutRebuilder.ForceRebuildLayoutImmediate (RectTransform);
+                Debug.Log ($"Items [{Index}].SetSize({i}) {lastSize} => {Size}: controlSize={m_controlChildSize}, localPosition={RectTransform.localPosition}, sizeDelta{RectTransform.sizeDelta}");
+                if (Mathf.Approximately (lastSize, Size)) {
+                    break;
+                }
+            }
+            Debug.Log ($"Items [{Index}].SetSize {Item.Size} => {Size}");
+            Item.Size = Size;
+        }
+
+        /// <summary>項目の位置決め</summary>
+        protected internal virtual void SetPosition (float pos) {
+            pos *= (vertical == m_reverseArrangement) ? 1f : -1f;
+            Vector2 anchor;
+            Vector2 anchoredPosition;
+            if (m_reverseArrangement) {
+                switch (m_childAlignment) {
+                    case TextAnchor.UpperRight:
+                        anchor.x = vertical ? 1f : 1f;
+                        anchor.y = vertical ? 0f : 1f;
+                        anchoredPosition.x = vertical ? -m_padding.right : pos;
+                        anchoredPosition.y = vertical ? pos : -m_padding.top;
+                        break;
+                    case TextAnchor.MiddleCenter:
+                        anchor.x = vertical ? 0.5f : 1f;
+                        anchor.y = vertical ? 0f : 0.5f;
+                        anchoredPosition.x = vertical ? 0f : pos;
+                        anchoredPosition.y = vertical ? pos : 0f;
+                        break;
+                    case TextAnchor.LowerLeft:
+                        anchor.x = vertical ? 0f : 1f;
+                        anchor.y = vertical ? 0f : 0f;
+                        anchoredPosition.x = vertical ? m_padding.left : pos;
+                        anchoredPosition.y = vertical ? pos : m_padding.bottom;
+                        break;
+                    default:
+                        anchor.x = 1f;
+                        anchor.y = 0f;
+                        anchoredPosition.x = 0f;
+                        anchoredPosition.y = 0f;
+                        break;
+                }
+            } else {
+                switch (m_childAlignment) {
+                    case TextAnchor.UpperRight:
+                        anchor.x = vertical ? 1f : 0f;
+                        anchor.y = vertical ? 1f : 1f;
+                        anchoredPosition.x = vertical ? -m_padding.right : pos;
+                        anchoredPosition.y = vertical ? pos : -m_padding.top;
+                        break;
+                    case TextAnchor.MiddleCenter:
+                        anchor.x = vertical ? 0.5f : 0f;
+                        anchor.y = vertical ? 1f : 0.5f;
+                        anchoredPosition.x = vertical ? 0f : pos;
+                        anchoredPosition.y = vertical ? pos : 0f;
+                        break;
+                    case TextAnchor.LowerLeft:
+                        anchor.x = vertical ? 0f : 0f;
+                        anchor.y = vertical ? 1f : 0f;
+                        anchoredPosition.x = vertical ? m_padding.left : pos;
+                        anchoredPosition.y = vertical ? pos : m_padding.bottom;
+                        break;
+                    default:
+                        anchor.x = 0f;
+                        anchor.y = 1f;
+                        anchoredPosition.x = 0f;
+                        anchoredPosition.y = 0f;
+                        break;
+                }
+            }
+            RectTransform.anchorMin = anchor;
+            RectTransform.anchorMax = anchor;
+            RectTransform.pivot = anchor;
+            RectTransform.anchoredPosition = anchoredPosition;
+            //Debug.Log ($"localPosition={RectTransform.localPosition}, pos={pos}");
+        }
+
+        /// <summary>文字列化</summary>
+        public override string ToString () => $"{base.ToString ()}({Index}, {Size}, {Item})";
+
+    }
+
+}
